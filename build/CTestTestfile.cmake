f647e8cc99734401141c33f4c78f8d331ed7593b
liponmollik <nura_azom@yahoo.com>
2025-02-22 14:05:56 +0600

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: C:/Work/CPP_APPS/bdOS
+# Build directory: C:/Work/CPP_APPS/bdOS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("aispecs")
+subdirs("aiiot")
+subdirs("clntsrvrcomm")
+subdirs("clouddsi")
+subdirs("contnrz")
+subdirs("iotspecs")
+subdirs("netcon")
+subdirs("secpriv")
+subdirs("serverspecs")
+subdirs("clientspecs")
+subdirs("tests")
